7235542c34f509e23999eda9beefbd922e38425c
Quacksire <sam@quacksire.dev>
2023-09-10 15:59:34 -0700

    Add Firebase configuration to api route
    This update adds Firebase configuration to the Next.js api route. It includes importing necessary modules, creating database and Firestore connections, handling instances for Firebase initialization, and setting up basic error handling. This allows for more robust server-side rendering with Firebase data and authentications.

---
+++ pages/api/[instanceId].ts
@@ -0,0 +1,61 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {ref, onValue, off, set} from 'firebase/database';
+
+
+import { getDatabase } from "@firebase/database"
+import { getFirestore } from "@firebase/firestore"
+import {useFirebaseApp} from "reactfire";
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCYhHEBH-bkXBADELuwQX4NsoqaH7460pA",
+    authDomain: "cider-collective.firebaseapp.com",
+    databaseURL: "https://connect-cidercollective.firebaseio.com",
+    projectId: "cider-collective",
+    storageBucket: "cider-collective.appspot.com",
+    messagingSenderId: "474254121753",
+    appId: "1:474254121753:web:a6d9e3568656d192820388",
+    measurementId: "G-Q3FL03JLBV",
+}
+// change the runtime configuration to nodejs
+// https://nextjs.org/docs/api-reference/next.config.js/runtime-configuration
+export const runtime = 'nodejs'
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+
+    // Use regular Firebase SDK functions to initialize the app
+    const app = initializeApp(firebaseConfig);
+
+    // initialize Database and Auth with the normal Firebase SDK functions
+    const database = getDatabase(app)
+    const firestore = getFirestore(app)
+
+
+    const { instanceId } = req.query;
+
+    if (!instanceId) {
+        res.status(400).json({ message: 'Missing instanceId' })
+        return;
+    }
+
+
+    const dbRef = ref(database, `instances/${instanceId}`);
+
+
+
+    const remoteRef = ref(database, `instances/${instanceId}/remote/online`);
+    await set(remoteRef, true);
+
+
+
+    onValue(dbRef, (snapshot) => {
+        const data = snapshot.val();
+
+        res.status(200).json({ ...data })
+    })
+
+
+}
